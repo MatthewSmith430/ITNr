ab1e2166740e99955e88a9fee8059485cc58140d
MatthewSmith430 <matt_smith.430@tiscali.co.uk>
2018-01-11 11:24:04 +0000

    jitter
    Add jitter to script

--- R/ITNimvex_function.R
+++ R/ITNimvex_function.R
@@ -23,7 +23,8 @@ ITNimvex<-function(gs,weighted){
   degree_df <- data.frame(deg.in, deg.out, str.out, str.in)
   if (weighted==TRUE){
     ggplot2::ggplot(degree_df, ggplot2::aes(x=str.in, y=str.out)) +
-      ggplot2::geom_point(shape=18, color="darkblue")+
+      ggplot2::geom_jitter(position = "jitter",shape=18, color="darkblue")+
+      #ggplot2::geom_point(shape=18, color="darkblue")+
       ggplot2::geom_smooth(method="lm",  linetype="dashed",
                   color="darkred")+
       ggplot2::labs(title="Imports Vs Exports (Strength)",
@@ -33,7 +34,8 @@ ITNimvex<-function(gs,weighted){
   }
   else{
     ggplot2::ggplot(degree_df, ggplot2::aes(x=deg.in, y=deg.out)) +
-      ggplot2::geom_point(shape=18, color="darkblue")+
+      ggplot2::geom_jitter(position = "jitter",shape=18, color="darkblue")+
+      #ggplot2::geom_point(shape=18, color="darkblue")+
       ggplot2::geom_smooth(method="lm",  linetype="dashed",
                   color="darkred")+
       ggplot2::labs(title="Imports Vs Exports (Count)",
